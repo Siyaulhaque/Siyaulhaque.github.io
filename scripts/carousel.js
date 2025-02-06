4f9d572a154ee311415f2a0f06d5e28d17c9db93
ziaulhaq <103656444+Siyaulhaque@users.noreply.github.com>
2025-02-06 19:08:47 +0530

    Update carousel.js

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -3,8 +3,10 @@ const totalSlides = 5; // Number of images
 
 function moveSlide(direction) {
     currentIndex += direction;
+    
     if (currentIndex < 0) currentIndex = totalSlides - 1;
     if (currentIndex >= totalSlides) currentIndex = 0;
+
     const slide = document.querySelector('.carousel-wrapper');
-    slide.style.transform = `translateX(-${currentIndex * 100}%)`;
+    slide.style.transform = `translateX(-${currentIndex * 20}%)`; // 20% per image
 }
